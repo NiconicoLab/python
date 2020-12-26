147933185e72804351ea4cb13a99bb35f52c61a8
NiconicoLab <lanesra.xxx@gmail.com>
2020-12-27 00:27:54 +0900

    modify gui sample code

--- gui.py
+++ gui.py
@@ -3,19 +3,25 @@ from tkinter import filedialog
 from tkinter import messagebox
 import os
 
+#グローバル変数(参照ファイルのパス)
+fpath = '/'
+
 #参照釦がクリックされたら実行
 def file_select():
     tkinter.messagebox.showinfo('タイトル','参照ファイルを選択してください')
     dir_path = os.path.dirname(os.path.abspath("__file__")) #ディレクトリの絶対パスを格納
-    print(dir_path)
+    #print(dir_path) #debug
     idir = dir_path #初期フォルダ
     filetype = [("テキスト","*.txt"), ("全て","*")] #拡張子
     file_path = tkinter.filedialog.askopenfilename(filetypes = filetype, initialdir = idir)
     input_box.insert(tkinter.END, file_path) #結果を表示
+    global fpath #グローバル変数であることを定義
+    fpath = file_path
 
 #実行釦がクリックされたら実行
 def file_conduct():
-    print("test")
+    print("実行!!!")
+    root.destroy()
 
 #ウインドウの作成
 root = tkinter.Tk()
@@ -39,3 +45,11 @@ button.place(x=10, y=200)
 
 #ウインドウの描画
 root.mainloop()
+
+#実行釦が押下された後に実行される
+if fpath == '/': #参照ファイルが指定されていない場合
+    print("file path error")
+else:
+    print(fpath)
+
+print("!!!end!!!")
