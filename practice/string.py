46674222b608757f62ae345bd64d097c64ae6a0f
NiconicoLab <lanesra.xxx@gmail.com>
2021-06-05 20:50:36 +0900

    add sample of string

---
+++ practice/string.py
@@ -0,0 +1,20 @@
+#文字列を複数行に渡って記載
+str = 'abc' \
+'def'
+print(str)
+
+#文字列結合
+str = 'a'
+str = str + 'b'
+str = str + 'c'
+print(str)
+
+#文字列結合
+str = 'a'
+str += 'b'
+str += 'c'
+print(str)
+
+#回数分繰り返し
+str = 'a' * 5
+print(str)
