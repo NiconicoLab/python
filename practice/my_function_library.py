1ba75b2c4e3d428bd64e775e1a6be0d015f2093f
NiconicoLab <lanesra.xxx@gmail.com>
2022-05-01 12:00:00 +0900

    add my function library

---
+++ practice/my_function_library.py
@@ -0,0 +1,10 @@
+'''
+自作関数のライブラリのお試しプログラム
+使う場合は下記のようにファイルをimportする
+import my_function_library
+* '-'をライブラリ名には含めない．syntaxエラーが出る *
+'''
+
+def print_lib(x):
+  print('my_function_library', x)
+  return x
