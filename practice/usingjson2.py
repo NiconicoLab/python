8de74e8aa80cd6542345a8e1e6788a96e695a70f
NiconicoLab <lanesra.xxx@gmail.com>
2022-05-10 12:00:00 +0900

    add if __name__ == '__main__'

--- practice/usingjson2.py
+++ practice/usingjson2.py
@@ -1,32 +1,34 @@
 import json
 
-fpath = './usingjson.json' #jsonファイルのパス
+if __name__ == '__main__':
 
-try:
-    with open(fpath) as db:
-        jsn = json.load(db) #jsonファイル読み込み
-        #print(jsn) #debug
-except IndexError:
-    print('not found')
+  fpath = './usingjson.json' #jsonファイルのパス
 
-for jsn_key in jsn:
-    print(jsn_key, end='')
-    print('     ', end='')
-    print(jsn[jsn_key]["parameter3"][0], end='')
-    print('     ', end='')
-    print(jsn[jsn_key]["parameter3"][1], end='')
-    print('     ', end='')
-    print(jsn[jsn_key]["parameter3"][2])
+  try:
+      with open(fpath) as db:
+          jsn = json.load(db) #jsonファイル読み込み
+          #print(jsn) #debug
+  except IndexError:
+      print('not found')
 
-import re
+  for jsn_key in jsn:
+      print(jsn_key, end='')
+      print('     ', end='')
+      print(jsn[jsn_key]["parameter3"][0], end='')
+      print('     ', end='')
+      print(jsn[jsn_key]["parameter3"][1], end='')
+      print('     ', end='')
+      print(jsn[jsn_key]["parameter3"][2])
 
-with open('./usingjson2.txt') as data:
-    lines = data.read()
-    for l in lines.split("\n"):
-        print(l)
-        words = re.split(" +", l) #正規表現モジュールで複数個のスペースに対応
-        if words[0]: #配列が空でない時
-          print(words, end='     ')
-          print(words[0], end=',')
-          print(words[1], end=',')
-          print(words[2])
+  import re
+
+  with open('./usingjson2.txt') as data:
+      lines = data.read()
+      for l in lines.split("\n"):
+          print(l)
+          words = re.split(" +", l) #正規表現モジュールで複数個のスペースに対応
+          if words[0]: #配列が空でない時
+            print(words, end='     ')
+            print(words[0], end=',')
+            print(words[1], end=',')
+            print(words[2])
