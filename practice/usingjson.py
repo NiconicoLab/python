81395e15c6db0ea1ba0d1020acc6b2da8a3096a5
NiconicoLab <lanesra.xxx@gmail.com>
2022-05-11 12:00:00 +0900

    add if __name__ == '__main__'

--- practice/usingjson.py
+++ practice/usingjson.py
@@ -1,31 +1,33 @@
 import json
 
-fpath = './usingjson.json' #jsonファイルのパス
+if __name__ == '__main__':
 
-try:
-    with open(fpath) as f:
-        jsn = json.load(f) #jsonファイル読み込み
-        #print(jsn) #debug
-except IndexError:
-    print('not found')
+  fpath = './usingjson.json' #jsonファイルのパス
 
-# jsonファイルのparameter2のみ表示
-print('--- test1 ---')
-for jsn_key in jsn:
-    print(jsn_key, end='')
-    print('     ', end='')
-    print(jsn[jsn_key]["parameter2"])
+  try:
+      with open(fpath) as f:
+          jsn = json.load(f) #jsonファイル読み込み
+          #print(jsn) #debug
+  except IndexError:
+      print('not found')
 
-# jsonファイルのparameter3の1つ目のデータのみ表示
-print('--- test2 ---')
-for jsn_key in jsn:
-    print(jsn_key, end='')
-    print('     ', end='')
-    print(jsn[jsn_key]["parameter3"][0])
+  # jsonファイルのparameter2のみ表示
+  print('--- test1 ---')
+  for jsn_key in jsn:
+      print(jsn_key, end='')
+      print('     ', end='')
+      print(jsn[jsn_key]["parameter2"])
 
-# jsonファイルのparameter3の2つ目のデータのみ表示
-print('--- test3 ---')
-for jsn_key in jsn:
-    print(jsn_key, end='')
-    print('     ', end='')
-    print(jsn[jsn_key]["parameter3"][1])
+  # jsonファイルのparameter3の1つ目のデータのみ表示
+  print('--- test2 ---')
+  for jsn_key in jsn:
+      print(jsn_key, end='')
+      print('     ', end='')
+      print(jsn[jsn_key]["parameter3"][0])
+
+  # jsonファイルのparameter3の2つ目のデータのみ表示
+  print('--- test3 ---')
+  for jsn_key in jsn:
+      print(jsn_key, end='')
+      print('     ', end='')
+      print(jsn[jsn_key]["parameter3"][1])
