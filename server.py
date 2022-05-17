3d364c2a16d035388c423ffb06f7778d72433292
NiconicoLab <lanesra.xxx@gmail.com>
2022-05-17 12:00:00 +0900

    add if __name__ == '__main__'

--- server.py
+++ server.py
@@ -1,20 +1,22 @@
 import socket
 import time
 
-sock = socket.socket(socket.AF_INET, type=socket.SOCK_DGRAM)
-sock.bind(('127.0.0.1', 12345))
+if __name__ == '__main__':
 
-while True:
-    try :
-        msg, addr = sock.recvfrom(128) # set receive size
-        msg = msg.decode(encoding='utf-8')
-        print(f'message : {msg}')
-
-        time.sleep(1)
-
-        # send response to client
-        sock.sendto('response success'.encode(encoding='utf-8'), addr)
-
-    except KeyboardInterrupt:
-        sock.close()
-        break
+  sock = socket.socket(socket.AF_INET, type=socket.SOCK_DGRAM)
+  sock.bind(('127.0.0.1', 12345))
+  
+  while True:
+      try :
+          msg, addr = sock.recvfrom(128) # set receive size
+          msg = msg.decode(encoding='utf-8')
+          print(f'message : {msg}')
+  
+          time.sleep(1)
+  
+          # send response to client
+          sock.sendto('response success'.encode(encoding='utf-8'), addr)
+  
+      except KeyboardInterrupt:
+          sock.close()
+          break
