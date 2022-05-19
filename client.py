ba4e5a1dcf2e1c8f26bd60a8a264b26357f60dba
NiconicoLab <lanesra.xxx@gmail.com>
2022-05-19 12:00:00 +0900

    add if __name__ == '__main__'

--- client.py
+++ client.py
@@ -1,22 +1,25 @@
 import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+if __name__ == '__main__':
 
-while True:
-    try:
-        print('input key. ([q]->exit)')
-        message = input()
-        if message != 'q':
-            len = sock.sendto(message.encode('utf-8'), ('127.0.0.1', 12345))
+  sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
 
-            # wait response from sercer
-            msg, addr = sock.recvfrom(128) # set receive size
-            print(f"{msg.decode(encoding='utf-8')}")
-
-        else:
-            sock.close()
-            break
-
-    except KeyboardInterrupt:
-        sock.close()
-        break
+  while True:
+      try:
+          print('input key. ([q]->exit)')
+          message = input()
+          if message != 'q':
+              len = sock.sendto(message.encode('utf-8'), ('127.0.0.1', 12345))
+  
+              # wait response from sercer
+              msg, addr = sock.recvfrom(128) # set receive size
+              print(f"{msg.decode(encoding='utf-8')}")
+  
+          else:
+              sock.close()
+              break
+  
+      except KeyboardInterrupt:
+          sock.close()
+          break
+  
