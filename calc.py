c96bfce522dad75004946c8dc3c7d513a0d21151
NiconicoLab <lanesra.xxx@gmail.com>
2022-05-20 12:00:00 +0900

    add if __name__ == '__main__'

--- calc.py
+++ calc.py
@@ -1,54 +1,55 @@
 # -*- coding: utf-8 -*-
 
-#四則演算
-print("** 四則演算 **")
-data1 = 1
-data2 = 5
-sum  = data1 + data2
-print(data1,'+',data2,'=',sum)
-
-#文字列制御
-print("** 文字列制御 **")
-str1 = 'Python '
-str2 = 'Verison '
-str3 = '3.7.0'
-print(str1 + str2 + str3)
-
-#for文
-print("** for文 **")
-str_array = ['string1', 'string2', 'string3']
-for name in str_array:
-    print(name)
-
-for name in str_array:
-    if name == "string1":
-        print(name)
-        print('BREAK')
-        break
-
-for name in str_array:
-    if name == "string1":
-        print('CONTINUE')
-        continue
-    print(name)
-
-for i in range(5):
-    print(i)
-
-#if文
-print("** if文 **")
-for i in range(5):
-    if i == 0:
-        print(i, ':' , 'i = 0')
-    elif 0 < i < 2:
-        print(i, ':' , '0 < i < 1')
-    else:
-        print(i, ':' , 'i > 1')
-
-#while文
-print("** while文 **")
-i = 0
-while i < 5:
-    print("i=", i, ':', str(i))
-    i += 1
-
+if __name__ == '__main__':
+  #四則演算
+  print("** 四則演算 **")
+  data1 = 1
+  data2 = 5
+  sum  = data1 + data2
+  print(data1,'+',data2,'=',sum)
+  
+  #文字列制御
+  print("** 文字列制御 **")
+  str1 = 'Python '
+  str2 = 'Verison '
+  str3 = '3.7.0'
+  print(str1 + str2 + str3)
+  
+  #for文
+  print("** for文 **")
+  str_array = ['string1', 'string2', 'string3']
+  for name in str_array:
+      print(name)
+  
+  for name in str_array:
+      if name == "string1":
+          print(name)
+          print('BREAK')
+          break
+  
+  for name in str_array:
+      if name == "string1":
+          print('CONTINUE')
+          continue
+      print(name)
+  
+  for i in range(5):
+      print(i)
+  
+  #if文
+  print("** if文 **")
+  for i in range(5):
+      if i == 0:
+          print(i, ':' , 'i = 0')
+      elif 0 < i < 2:
+          print(i, ':' , '0 < i < 1')
+      else:
+          print(i, ':' , 'i > 1')
+  
+  #while文
+  print("** while文 **")
+  i = 0
+  while i < 5:
+      print("i=", i, ':', str(i))
+      i += 1
+  
