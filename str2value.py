4b5950863f92fbf3578da87ad2cc729331473fde
NiconicoLab <lanesra.xxx@gmail.com>
2022-06-04 12:00:00 +0900

    add program converted string to value

---
+++ str2value.py
@@ -0,0 +1,27 @@
+
+'''
+文字列から数値への変換
+'''
+str = '11'
+n10 = int(str, 10)  # 文字列を10進として数値に変換
+n16 = int(str, 16)  # 文字列を16進として数値に変換
+
+print('Convert string to value')
+print(n10, end=',')
+print(n16)
+
+'''
+数値から文字列への変換
+'''
+
+val = 255
+str2   = format(val, 'b') # 数値を2進として文字列に変換
+str8   = format(val, 'o') # 数値を8進として文字列に変換
+str10 = format(val, 'd') # 数値を10進として文字列に変換
+str16 = format(val, 'X') # 数値を16進として文字列に変換
+
+print('Convert value to string')
+print(str2, end=',')
+print(str8, end=',')
+print(str10, end=',')
+print(str16)
